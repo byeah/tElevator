4322d1bed6614ef37cc73b7c5e9692727a50ccda
byeah <jby.yeah@gmail.com>
2013-01-25 23:46:55 +0800

    Sample project setting

---
+++ src/main/scala/Hello/add.scala
@@ -0,0 +1,14 @@
+package Hello
+
+
+object add
+{
+	
+	def add(x:Int,y:Int)= x+y
+		
+	def main(args:Array[String])
+	{
+		println("OK")
+		println(add(2,4))
+	}
+}
---
+++ src/test/scala/Hello/helloFunSuite.scala
@@ -0,0 +1,16 @@
+package Hello
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+@RunWith(classOf[JUnitRunner])
+class helloFunSuite extends FunSuite
+{
+	test("hello")
+	{
+		assert(1+1==2)
+		assert(add.add(1, 1)==2)
+	}
+}
+
