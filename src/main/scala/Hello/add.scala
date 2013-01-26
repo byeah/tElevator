70930d7ddc5f334b049a4c1e6149c6d29bb4580a
byeah <jby.yeah@gmail.com>
2013-01-26 11:40:02 +0800

    This is an addition...

--- src/main/scala/Hello/add.scala
+++ src/main/scala/Hello/add.scala
@@ -1,14 +1,31 @@
+/**
+ * @author BYeah
+ */
 package Hello
 
+class myClass
+{
+	def a() = 3
+}
 
 object add
 {
-	
+	/**
+	 * This is a simple add method
+	 * @param x The first
+	 * @param y The second
+	 * @return The sum of x and y
+	 */
 	def add(x:Int,y:Int)= x+y
-		
+			
+	
+	/**
+	 * This is something...
+	 */
 	def main(args:Array[String])
 	{
 		println("OK")
 		println(add(2,4))
+		println("Hello World")
 	}
 }
