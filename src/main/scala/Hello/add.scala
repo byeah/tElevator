56b063020b0ebdd7634565f64f869c0aa944b570
byeah <jby.yeah@gmail.com>
2013-01-26 11:16:41 +0800

    Another sample project using scaladoc+IntelliJ

--- src/main/scala/Hello/add.scala
+++ src/main/scala/Hello/add.scala
@@ -1,14 +1,27 @@
+/**
+ * @author BYeah
+ */
 package Hello
 
+class myClass
+{
+	def a() = 3
+}
 
 object add
 {
-	
+	/**
+	 * This is a simple add method
+	 * @param x The first
+	 * @param y The second
+	 * @return The sum of x and y
+	 */
 	def add(x:Int,y:Int)= x+y
-		
+
 	def main(args:Array[String])
 	{
 		println("OK")
 		println(add(2,4))
+		println("Hello World")
 	}
 }
